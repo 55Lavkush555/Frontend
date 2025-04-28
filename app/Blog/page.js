1c1b41d6c186b838dd4152ce31d8c5c2ca53d709
Lavkush <155431474+55Lavkush555@users.noreply.github.com>
2025-04-28 12:17:03 +0530

    Add files via upload

--- app/Blog/page.js
+++ app/Blog/page.js
@@ -4,6 +4,9 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 
 const Blog = () => {
+  const [search, setSearch] = useState("")
+  const [isSearch, setIsSearch] = useState(false)
+  const [searchReselt, setSearchReselt] = useState(0)
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -20,10 +23,19 @@ const Blog = () => {
     fetchPosts();
   }, [])
 
+  const handleSearch = () => {
+    setIsSearch(true)
+  }
+
 
   return (
     <div className='container mx-auto pt-5'>
-      {posts.length > 0 ? (
+      <div className="search flex gap-1.5 items-center flex-wrap justify-center">
+        <input value={search} onChange={(e) => { setSearch(e.target.value); setSearchReselt(0) }} type="text" placeholder='Type to search' className='border-2 rounded-full px-1.5 focus:border-red-600' />
+        <button className='text-[20px] bg-red-600 rounded-full px-5 text-white focus:bg-red-700 hover:bg-red-700 cursor-pointer w-fit' onClick={handleSearch}>Search</button>
+      </div>
+
+      {posts.length > 0 && isSearch == false ? (
         posts.map((post, index) => (
           <div key={index} className="relative card my-5 p-4 border border-gray-300 rounded-xl shadow-md font-sans max-h-48 overflow-hidden">
             <h1 className='text-2xl font-semibold'>{post.title}</h1>
@@ -34,8 +46,32 @@ const Blog = () => {
           </div>
         ))
       ) : (
-        <p className='text-center text-gray-500'>Loading blogs...</p>
+        <>
+          {isSearch == false && (
+            <p className='text-center text-gray-500'>Loading blogs...</p>
+          )}
+        </>
       )}
+
+      {isSearch && (
+        <>
+          <h2 className='text-2xl text-gray-500'>Search Reasult for "{search}"</h2>
+          {posts.map((post, index) => (
+            <div key={index}>
+              {post.title.toLowerCase().includes(search) && (
+                <div className="relative card my-5 p-4 border border-gray-300 rounded-xl shadow-md font-sans max-h-48 overflow-hidden">
+                  <h1 className='text-2xl font-semibold'>{post.title}</h1>
+                  <p className='text-gray-400 text-sm mb-2'>{post.date || 'No Date Provided'}</p>
+                  <pre className='text-base font-sans' dangerouslySetInnerHTML={{ __html: post.content }}>
+                  </pre>
+                  <Link href={`/Blog/${post.title}`} className='absolute bottom-1 right-0'><button className='text-[20px] bg-red-600 rounded-full px-5 mr-2.5 text-white mt-2.5 focus:bg-red-700 hover:bg-red-700 cursor-pointer w-fit'>Read</button></Link>
+                </div>
+              )}
+            </div>
+          ))}
+        </>
+      )}
+
     </div>
   )
 }
