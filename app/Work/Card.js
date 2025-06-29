519940dfcb4ad84b5fbf7999d639469cc48e867c
Lavkush <155431474+55Lavkush555@users.noreply.github.com>
2025-06-29 02:26:09 -0700

    Update Card.js

--- app/Work/Card.js
+++ app/Work/Card.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const Card = ({head, body, img_link, project_link}) => {
     return (
         <div className="card flex flex-col items-center mt-5 card my-5 p-4 sm:border border-gray-400 rounded-xl shadow-md">
-            <Image className='object-cover' src={img_link} alt='' width={450} height={300} loading='lazy' />
+            <Image className='object-cover border-2' src={img_link} alt='' width={450} height={300} loading='lazy' />
             <h1 className="text-2xl font-bold py-1.5">{head}</h1>
             <p className='px-3.5 text-[17.5px]'>{body}</p>
             <div className="btn w-full px-3.5 flex justify-center items-center">
